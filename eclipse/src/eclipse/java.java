08b61b749e1f9548159e81bab1e3623204cfcb7f
CW <CW@192.168.10.12>
2024-11-03 18:54:07 +0500

    add main class

---
+++ eclipse/src/eclipse/java.java
@@ -0,0 +1,14 @@
+package eclipse;
+
+public class java {
+
+	public java() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
